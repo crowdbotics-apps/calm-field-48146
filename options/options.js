ad66601148b58c37a914320d31d6836b3a78c58a
Crowdbotics <team@crowdbotics.com>
2024-05-28 15:37:00 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "calm-field-48146",
+  url: "https://calm-field-48146.botics.co",
+  api: "https://calm-field-48146.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
